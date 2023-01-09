4ca31ab89760ba8136022c49df8703b2fab3e45c
Gloria <gloria.kitili@student.moringaschool.com>
2023-01-10 02:31:08 +0300

    completed tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import Article from "../components/Article";
+import Header from "../components/Header";
 
 function App() {
   return <div className="App">
